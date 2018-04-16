513d47f2e832b6ca104fa706921b7a56552a341c
Tuan Duong <dmtuan1995@gmail.com>
2018-04-16 18:55:20 +0700

    Delaration of string, Lenght of string and Edit a string

--- Lession-1/string.js
+++ Lession-1/string.js
@@ -1,2 +1,13 @@
 var example = "this is a string";
 console.log (example);
+var example_1 = "this is also a string";
+console.log (example_1);
+
+
+var example_lenght = example.length;
+console.log (example_lenght);
+var example_1_lenght = example_1.length;
+console.log (example_1_lenght);
+
+example = example.replace ('string' , 'example');
+console.log (example);
